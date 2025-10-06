c73d39ac885d8cbfd6a99994edb0620a4e0cd308
Antony Teoh <antony.teoh@gmail.com>
2025-10-06 15:23:38 +0100

    PAE-275: Update ESLint and use neostandard (#11)

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import neostandard from 'neostandard'
+
+const ns = neostandard({
+  env: ['node', 'es2022', 'jest'],
+  files: ['test/**/*.js'],
+  ignores: [
+    ...neostandard.resolveIgnoresFromGitignore(),
+    'allure-results/',
+    'allure-report/',
+    'docker/',
+    'wdio*.conf.js'
+  ],
+  noJsx: true,
+  noStyle: true
+})
+
+// Fix to override ecmaVersion for import attributes support, see related issue:
+// https://github.com/neostandard/neostandard/issues/307
+for (const item of ns) {
+  if (item?.languageOptions?.ecmaVersion < 2025) {
+    item.languageOptions.ecmaVersion = 2025
+  }
+}
+
+export default ns
