5da6ef81504803b082c228e6a129fee56b42bcfe
Antony Teoh <antony.teoh@gmail.com>
2025-11-12 16:58:45 +0000

    PAE-352: Add Organisation details JSON editor test

--- test/specs/organisations.e2e.js
+++ test/specs/organisations.e2e.js
@@ -3,6 +3,7 @@ import { browser, expect } from '@wdio/globals'
 import LoginPage from 'page-objects/login.js'
 import Navigation from 'page-objects/navigation.js'
 import OrganisationsPage from 'page-objects/organisations.js'
+import JsonEditor from 'page-objects/jsoneditor.js'
 
 //TODO: Enable this test once the backend is ready
 describe('Organisations page', () => {
@@ -16,11 +17,26 @@ describe('Organisations page', () => {
     expect(headerText).toEqual('Welcome EA Regulator!')
 
     await Navigation.clickOnLink('Organisations')
+
+    await OrganisationsPage.editLink(1)
+
+    await JsonEditor.switchToTextEditor()
+    const actualOrgValue = await JsonEditor.getEditorTextValue()
+    expect(actualOrgValue).toContain('anakin.skywalker@starwars.com')
+    await JsonEditor.switchToTreeEditor()
+    await JsonEditor.updateOrgId('50006')
+    await JsonEditor.saveChanges()
+
+    const successMessage = await OrganisationsPage.getSuccessMessage()
+    expect(successMessage).toEqual('Organisation record updated')
+
+    await Navigation.clickOnLink('Organisations')
+
     const actualOrgTable = await OrganisationsPage.getTableData()
     const expectedOrgTable = [
       {
         header: 'ACME ltd',
-        orgId: '50002',
+        orgId: '50006',
         regNo: 'AC012345',
         status: 'created'
       },
