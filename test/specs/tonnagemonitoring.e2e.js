25c6094852fc3573022f3e6a869402152e04f26d
Antony Teoh <antony.teoh@gmail.com>
2026-02-04 15:13:47 +0000

    Skip tonnage monitoring tests for now and update expectations

--- test/specs/tonnagemonitoring.e2e.js
+++ test/specs/tonnagemonitoring.e2e.js
@@ -5,7 +5,7 @@ import Navigation from 'page-objects/navigation.js'
 import TonnageMonitoringPage from 'page-objects/tonnage.monitoring.page.js'
 
 describe('Tonnage Monitoring page', () => {
-  it('Should be able to view Tonnage Monitoring if logged in @tonnagemonitoring', async () => {
+  it.skip('Should be able to view Tonnage Monitoring if logged in @tonnagemonitoring', async () => {
     await LoginPage.open()
     await expect(browser).toHaveTitle(expect.stringContaining('Login'))
     await LoginPage.enterCredentials('ea@test.gov.uk', 'pass')
@@ -15,9 +15,10 @@ describe('Tonnage Monitoring page', () => {
 
     const expectedTable = [
       { Material: 'Aluminium', Tonnage: '0.00' },
-      { Material: 'Fibre', Tonnage: '0.00' },
-      { Material: 'Glass', Tonnage: '0.00' },
-      { Material: 'Paper', Tonnage: '0.00' },
+      { Material: 'Fibre based composite ', Tonnage: '0.00' },
+      { Material: 'Glass other', Tonnage: '0.00' },
+      { Material: 'Glass re-melt', Tonnage: '0.00' },
+      { Material: 'Paper and board', Tonnage: '0.00' },
       { Material: 'Plastic', Tonnage: '0.00' },
       { Material: 'Steel', Tonnage: '0.00' },
       { Material: 'Wood', Tonnage: '0.00' }
