2ddd4bd17aedd8c4a6c006c664306b2b276989ed
Antony Teoh <antony.teoh@gmail.com>
2026-02-10 12:30:30 +0000

    Update Tonnage monitoring test (#65)

--- test/specs/tonnagemonitoring.e2e.js
+++ test/specs/tonnagemonitoring.e2e.js
@@ -5,7 +5,7 @@ import Navigation from 'page-objects/navigation.js'
 import TonnageMonitoringPage from 'page-objects/tonnage.monitoring.page.js'
 
 describe('Tonnage Monitoring page', () => {
-  it.skip('Should be able to view Tonnage Monitoring if logged in @tonnagemonitoring', async () => {
+  it('Should be able to view Tonnage Monitoring if logged in @tonnagemonitoring', async () => {
     await LoginPage.open()
     await expect(browser).toHaveTitle(expect.stringContaining('Login'))
     await LoginPage.enterCredentials('ea@test.gov.uk', 'pass')
@@ -15,13 +15,13 @@ describe('Tonnage Monitoring page', () => {
 
     const expectedTable = [
       { Material: 'Aluminium', Tonnage: '0.00' },
-      { Material: 'Fibre based composite ', Tonnage: '0.00' },
-      { Material: 'Glass other', Tonnage: '0.00' },
-      { Material: 'Glass re-melt', Tonnage: '0.00' },
+      { Material: 'Fibre based composite', Tonnage: '0.00' },
       { Material: 'Paper and board', Tonnage: '0.00' },
       { Material: 'Plastic', Tonnage: '0.00' },
       { Material: 'Steel', Tonnage: '0.00' },
-      { Material: 'Wood', Tonnage: '0.00' }
+      { Material: 'Wood', Tonnage: '0.00' },
+      { Material: 'Glass re-melt', Tonnage: '0.00' },
+      { Material: 'Glass other', Tonnage: '0.00' }
     ]
     const materialTableData =
       await TonnageMonitoringPage.tonnageMaterialTableData()
