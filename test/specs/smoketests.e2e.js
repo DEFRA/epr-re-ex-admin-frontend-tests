a62cb59a9e2d0db8fcd502a6f65f5a86f4d685da
Antony Teoh <antony.teoh@gmail.com>
2026-02-13 16:41:15 +0000

    PAE-000: Update smoke tests to make it more reliable (#75)

--- test/specs/smoketests.e2e.js
+++ test/specs/smoketests.e2e.js
@@ -8,6 +8,7 @@ import PublicRegisterPage from 'page-objects/public.register.page.js'
 import SystemLogsPage from 'page-objects/system.logs.page.js'
 import config from '../config/config.js'
 import TonnageMonitoringPage from 'page-objects/tonnage.monitoring.page.js'
+import { checkBodyText } from '~/test/support/check.js'
 
 describe('Smoke tests @smoketest', () => {
   it('Should be to login and view Home Page and Organisations Page', async () => {
@@ -24,6 +25,8 @@ describe('Smoke tests @smoketest', () => {
     expect(headerText).toEqual('Welcome ServiceMaintainer TestUser (Defra)!')
 
     await Navigation.clickOnLink('Organisations')
+    // Give it some time as organisations page might take time to load
+    checkBodyText('All organisations', 60)
     const orgTableHeader = await OrganisationsPage.getHeaderText()
     expect(orgTableHeader).toBe('All organisations')
 
@@ -32,6 +35,8 @@ describe('Smoke tests @smoketest', () => {
     expect(actualNoSystemLogsFoundText).toEqual('No system logs found')
 
     await Navigation.clickOnLink('Public register')
+    // Give it some time to load
+    checkBodyText('Public register', 30)
     await PublicRegisterPage.downloadPublicRegister()
 
     await Navigation.clickOnLink('Tonnage monitoring')
