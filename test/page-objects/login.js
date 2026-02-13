ce9d2094f21b176feb78e6b60179f16bc6aef53d
Antony Teoh <antony.teoh@gmail.com>
2026-02-13 17:11:17 +0000

    Add timeout to smoke test, remove try/catch

--- test/page-objects/login.js
+++ test/page-objects/login.js
@@ -24,14 +24,9 @@ class LoginPage extends Page {
     const nextPWButton = await $('input[value="Sign in"]')
     await nextPWButton.click()
 
-    try {
-      const submitElement = await $('input[value="Yes"]')
-      await submitElement.waitForExist({ timeout: 5000 })
-      await submitElement.click()
-    } catch (error) {
-      // Element didn't appear, continue without clicking
-      console.log('"Yes" button not found on MS Login, skipping...')
-    }
+    const submitElement = await $('input[value="Yes"]')
+    await submitElement.waitForExist({ timeout: 5000 })
+    await submitElement.click()
   }
 
   async submitCredentials() {
