999cb2cbcab455fe5150627e55cfdd8bb20d05a5
Tom Halley <2283407+tomhalley@users.noreply.github.com>
2026-02-26 11:29:48 +0000

    PAE-000: align localstack startup scripts with frontend tests

--- docker/scripts/localstack/01-start-localstack.sh
+++ docker/scripts/localstack/01-start-localstack.sh
@@ -10,7 +10,6 @@ export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 
-
 echo "[INIT SCRIPT] Creating buckets" >&2
 
 aws --endpoint-url=http://localhost:4566 s3 mb s3://re-ex-public-register
