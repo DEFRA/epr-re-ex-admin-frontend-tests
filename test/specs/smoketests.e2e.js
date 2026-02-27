3f7d4c5ef21a30aee814dcb5311f3582df09ca67
Tony Bruce <t.bruce.mail@googlemail.com>
2026-02-27 15:32:31 +0000

    Update smoketests.e2e.js - Added PAE-433 test
    Redirected to which page they intended to sign-in test

--- test/specs/smoketests.e2e.js
+++ test/specs/smoketests.e2e.js
@@ -40,4 +40,21 @@ describe('Smoke tests @smoketest', () => {
     await Navigation.clickOnLink('Tonnage monitoring')
     await TonnageMonitoringPage.downloadCsv()
   })
+
+  it('Should be redirected to the orginially requested page and not redirected to the home page', async () => {
+    await browser.setTimeout({ pageLoad: 60000 })
+
+    await OrganisationsPage.open()
+    await expect(browser).toHaveTitle(expect.stringContaining('Organisations'))
+
+    await $('=Sign in').click()
+
+    await LoginPage.enterCredentials('ea@test.gov.uk', 'pass')
+    await LoginPage.submitCredentials()
+
+    const orgTableHeader = await OrganisationsPage.getHeaderText()
+    expect(orgTableHeader).toBe('All organisations')
+
+    await expect($('body')).not.toHaveText(expect.stringContaining('This is the home page.'))
+  })
 })
