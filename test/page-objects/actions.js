688ab85a58afa8fc81b5b060bb6563f7f34fb6c9
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-04-14 17:38:16 +0100

    PAE-000: include url in wait helper timeout messages

--- test/page-objects/actions.js
+++ test/page-objects/actions.js
@@ -1,17 +1,27 @@
-import { $ } from '@wdio/globals'
+import { $, browser } from '@wdio/globals'
 
 const DEFAULT_TIMEOUT = 10000
 
 const resolve = (target) => (typeof target === 'string' ? $(target) : target)
 
+const waitWith = async (waitFn, timeoutMsg) => {
+  try {
+    await waitFn({ timeout: DEFAULT_TIMEOUT, timeoutMsg })
+  } catch (err) {
+    const url = await browser.getUrl().catch(() => '(unknown)')
+    err.message = `${err.message} (at ${url})`
+    throw err
+  }
+}
+
 export const clickWhenReady = async (target, timeoutMsg) => {
   const element = await resolve(target)
-  await element.waitForClickable({ timeout: DEFAULT_TIMEOUT, timeoutMsg })
+  await waitWith((opts) => element.waitForClickable(opts), timeoutMsg)
   return element.click()
 }
 
 export const setValueWhenReady = async (target, value, timeoutMsg) => {
   const element = await resolve(target)
-  await element.waitForDisplayed({ timeout: DEFAULT_TIMEOUT, timeoutMsg })
+  await waitWith((opts) => element.waitForDisplayed(opts), timeoutMsg)
   return element.setValue(value)
 }
