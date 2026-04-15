4ebca8400e84462a74f8a0eebf3c79d3072814d0
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-04-15 10:20:40 +0100

    PAE-000: chain link-text selector under navigation element

--- test/page-objects/navigation.js
+++ test/page-objects/navigation.js
@@ -1,9 +1,11 @@
+import { $ } from '@wdio/globals'
+
 import { clickWhenReady } from 'page-objects/actions'
 
 class Navigation {
   async clickOnLink(linkText) {
     await clickWhenReady(
-      `#navigation a=${linkText}`,
+      $('#navigation').$(`=${linkText}`),
       `Navigation link "${linkText}" not clickable`
     )
   }
