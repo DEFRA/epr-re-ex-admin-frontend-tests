aa0cc2244d5eca74be2a54e9336a7916abc61f13
Antony Teoh <antony.teoh@gmail.com>
2026-06-12 13:38:50 +0100

    Force run 1 scenario only

--- test/specs/organisations.e2e.js
+++ test/specs/organisations.e2e.js
@@ -22,7 +22,7 @@ describe('Organisations page', () => {
     await LoginPage.submitCredentials()
   })
 
-  it('Should be able to update an organisation and view system logs @organisations', async () => {
+  it('Should be able to update an organisation and view system logs @testonly @organisations', async () => {
     const linkedOrganisation = await createLinkedOrganisation([
       { material: 'Paper or board (R3)', wasteProcessingType: 'Reprocessor' },
       { material: 'Paper or board (R3)', wasteProcessingType: 'Exporter' }
