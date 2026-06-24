1af0da31f808542e780a4cc4c387ba25ebfed75a
Antony Teoh <antony.teoh@gmail.com>
2026-06-24 10:45:25 +0100

    Scroll into view

--- test/specs/organisations.e2e.js
+++ test/specs/organisations.e2e.js
@@ -239,6 +239,12 @@ describe('Organisations page', () => {
     expect(reportsData[lastRowIdx].actions).toContain('View')
     expect(reportsData[lastRowIdx].actions).toContain('Unsubmit')
 
+    await browser.scrollIntoView(
+      await $(
+        `#reports > table > tbody > tr:nth-child(${lastRowIdx + 1}) > td:nth-child(4) > a[href*="unsubmit"]`
+      )
+    )
+
     // unsubmit report
     // await RegistrationOverviewPage.clickOnUnsubmitReportLink(lastRowIdx + 1)
     const warningText = await UnsubmitConfirmationPage.getWarningText()
